98cd9426b435c5ce687eeed5927ed9c04a7d0ad2
Mohamed Achaq <motriyanox@gmail.com>
2024-12-10 11:52:43 +0100

    feat: move to `protobuff` for messages communication

--- lla_plugin_interface/src/lib.rs
+++ lla_plugin_interface/src/lib.rs
@@ -1,6 +1,11 @@
+use bytes::BytesMut;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::path::PathBuf;
+
+pub mod proto {
+    include!(concat!(env!("OUT_DIR"), "/lla_plugin.rs"));
+}
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct DecoratedEntry {
@@ -46,8 +51,144 @@ pub enum PluginResponse {
     Error(String),
 }
 
+impl From<EntryMetadata> for proto::EntryMetadata {
+    fn from(meta: EntryMetadata) -> Self {
+        proto::EntryMetadata {
+            size: meta.size,
+            modified: meta.modified,
+            accessed: meta.accessed,
+            created: meta.created,
+            is_dir: meta.is_dir,
+            is_file: meta.is_file,
+            is_symlink: meta.is_symlink,
+            permissions: meta.permissions,
+            uid: meta.uid,
+            gid: meta.gid,
+        }
+    }
+}
+
+impl From<proto::EntryMetadata> for EntryMetadata {
+    fn from(meta: proto::EntryMetadata) -> Self {
+        EntryMetadata {
+            size: meta.size,
+            modified: meta.modified,
+            accessed: meta.accessed,
+            created: meta.created,
+            is_dir: meta.is_dir,
+            is_file: meta.is_file,
+            is_symlink: meta.is_symlink,
+            permissions: meta.permissions,
+            uid: meta.uid,
+            gid: meta.gid,
+        }
+    }
+}
+
+impl From<DecoratedEntry> for proto::DecoratedEntry {
+    fn from(entry: DecoratedEntry) -> Self {
+        proto::DecoratedEntry {
+            path: entry.path.to_string_lossy().to_string(),
+            metadata: Some(entry.metadata.into()),
+            custom_fields: entry.custom_fields,
+        }
+    }
+}
+
+impl TryFrom<proto::DecoratedEntry> for DecoratedEntry {
+    type Error = std::io::Error;
+
+    fn try_from(entry: proto::DecoratedEntry) -> Result<Self, Self::Error> {
+        Ok(DecoratedEntry {
+            path: PathBuf::from(entry.path),
+            metadata: entry.metadata.unwrap_or_default().into(),
+            custom_fields: entry.custom_fields,
+        })
+    }
+}
+
 pub trait Plugin: Send + Sync {
     fn handle_request(&mut self, request: PluginRequest) -> PluginResponse;
+
+    fn handle_raw_request(&mut self, request: &[u8]) -> Vec<u8> {
+        use prost::Message;
+
+        let proto_msg = match proto::PluginMessage::decode(request) {
+            Ok(msg) => msg,
+            Err(e) => return encode_error(&format!("Failed to decode request: {}", e)),
+        };
+
+        let request = match proto_to_request(proto_msg) {
+            Ok(req) => req,
+            Err(e) => return encode_error(&format!("Failed to convert request: {}", e)),
+        };
+
+        let response = self.handle_request(request);
+
+        let proto_response = response_to_proto(response);
+        let mut buf = BytesMut::with_capacity(proto_response.encoded_len());
+        proto_response.encode(&mut buf).unwrap();
+        buf.to_vec()
+    }
+}
+
+fn proto_to_request(msg: proto::PluginMessage) -> Result<PluginRequest, std::io::Error> {
+    use proto::plugin_message::Message;
+
+    match msg.message {
+        Some(Message::GetName(_)) => Ok(PluginRequest::GetName),
+        Some(Message::GetVersion(_)) => Ok(PluginRequest::GetVersion),
+        Some(Message::GetDescription(_)) => Ok(PluginRequest::GetDescription),
+        Some(Message::GetSupportedFormats(_)) => Ok(PluginRequest::GetSupportedFormats),
+        Some(Message::Decorate(entry)) => Ok(PluginRequest::Decorate(entry.try_into()?)),
+        Some(Message::FormatField(req)) => Ok(PluginRequest::FormatField(
+            req.entry.unwrap_or_default().try_into()?,
+            req.format,
+        )),
+        Some(Message::Action(req)) => Ok(PluginRequest::PerformAction(req.action, req.args)),
+        _ => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Invalid request type",
+        )),
+    }
+}
+
+fn response_to_proto(response: PluginResponse) -> proto::PluginMessage {
+    use proto::plugin_message::Message;
+
+    let message = match response {
+        PluginResponse::Name(name) => Message::NameResponse(name),
+        PluginResponse::Version(version) => Message::VersionResponse(version),
+        PluginResponse::Description(desc) => Message::DescriptionResponse(desc),
+        PluginResponse::SupportedFormats(formats) => {
+            Message::FormatsResponse(proto::SupportedFormatsResponse { formats })
+        }
+        PluginResponse::Decorated(entry) => Message::DecoratedResponse(entry.into()),
+        PluginResponse::FormattedField(field) => {
+            Message::FieldResponse(proto::FormattedFieldResponse { field })
+        }
+        PluginResponse::ActionResult(result) => Message::ActionResponse(proto::ActionResponse {
+            success: result.is_ok(),
+            error: result.err(),
+        }),
+        PluginResponse::Error(error) => Message::ErrorResponse(error),
+    };
+
+    proto::PluginMessage {
+        message: Some(message),
+    }
+}
+
+fn encode_error(error: &str) -> Vec<u8> {
+    use prost::Message;
+    let response = proto::PluginMessage {
+        message: Some(proto::plugin_message::Message::ErrorResponse(
+            error.to_string(),
+        )),
+    };
+    let mut buf = BytesMut::with_capacity(response.encoded_len());
+    response.encode(&mut buf).unwrap();
+    buf.to_vec()
 }
 
 #[macro_export]
