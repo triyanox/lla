d60b5b432eef574cb0505f7e65ea69408d864ada
Mohamed Achaq <motriyanox@gmail.com>
2024-12-11 00:54:07 +0100

    feat: migrate `dirs` to `protobuf`

--- plugins/dirs/src/lib.rs
+++ plugins/dirs/src/lib.rs
@@ -1,7 +1,11 @@
 use colored::Colorize;
 use lazy_static::lazy_static;
-use lla_plugin_interface::{Plugin, PluginRequest, PluginResponse};
+use lla_plugin_interface::{
+    proto::{self, plugin_message::Message},
+    Plugin,
+};
 use parking_lot::RwLock;
+use prost::Message as _;
 use rayon::prelude::*;
 use std::collections::HashMap;
 use std::path::Path;
@@ -96,19 +100,44 @@ impl Default for DirsPlugin {
 }
 
 impl Plugin for DirsPlugin {
-    fn handle_request(&mut self, request: PluginRequest) -> PluginResponse {
-        match request {
-            PluginRequest::GetName => PluginResponse::Name(env!("CARGO_PKG_NAME").to_string()),
-            PluginRequest::GetVersion => {
-                PluginResponse::Version(env!("CARGO_PKG_VERSION").to_string())
+    fn handle_raw_request(&mut self, request: &[u8]) -> Vec<u8> {
+        let proto_msg = match proto::PluginMessage::decode(request) {
+            Ok(msg) => msg,
+            Err(e) => {
+                let error_msg = proto::PluginMessage {
+                    message: Some(Message::ErrorResponse(format!(
+                        "Failed to decode request: {}",
+                        e
+                    ))),
+                };
+                let mut buf = bytes::BytesMut::with_capacity(error_msg.encoded_len());
+                error_msg.encode(&mut buf).unwrap();
+                return buf.to_vec();
             }
-            PluginRequest::GetDescription => {
-                PluginResponse::Description(env!("CARGO_PKG_DESCRIPTION").to_string())
+        };
+
+        let response_msg = match proto_msg.message {
+            Some(Message::GetName(_)) => Message::NameResponse(env!("CARGO_PKG_NAME").to_string()),
+            Some(Message::GetVersion(_)) => {
+                Message::VersionResponse(env!("CARGO_PKG_VERSION").to_string())
             }
-            PluginRequest::GetSupportedFormats => {
-                PluginResponse::SupportedFormats(vec!["default".to_string(), "long".to_string()])
+            Some(Message::GetDescription(_)) => {
+                Message::DescriptionResponse(env!("CARGO_PKG_DESCRIPTION").to_string())
             }
-            PluginRequest::Decorate(mut entry) => {
+            Some(Message::GetSupportedFormats(_)) => {
+                Message::FormatsResponse(proto::SupportedFormatsResponse {
+                    formats: vec!["default".to_string(), "long".to_string()],
+                })
+            }
+            Some(Message::Decorate(entry)) => {
+                let mut entry = match lla_plugin_interface::DecoratedEntry::try_from(entry.clone())
+                {
+                    Ok(e) => e,
+                    Err(e) => {
+                        return self.encode_error(&format!("Failed to convert entry: {}", e));
+                    }
+                };
+
                 if entry.metadata.is_dir {
                     if let Some((file_count, dir_count, total_size)) =
                         Self::analyze_directory(&entry.path)
@@ -124,67 +153,104 @@ impl Plugin for DirsPlugin {
                             .insert("dir_total_size".to_string(), Self::format_size(total_size));
                     }
                 }
-                PluginResponse::Decorated(entry)
+                Message::DecoratedResponse(entry.into())
             }
-            PluginRequest::FormatField(entry, format) => {
-                if !entry.metadata.is_dir {
-                    return PluginResponse::FormattedField(None);
-                }
-
-                let formatted = match format.as_str() {
-                    "long" => {
-                        if let (Some(file_count), Some(dir_count), Some(total_size)) = (
-                            entry.custom_fields.get("dir_file_count"),
-                            entry.custom_fields.get("dir_subdir_count"),
-                            entry.custom_fields.get("dir_total_size"),
-                        ) {
-                            let modified = entry
-                                .path
-                                .metadata()
-                                .ok()
-                                .and_then(|m| m.modified().ok())
-                                .and_then(|t| t.elapsed().ok())
-                                .map(|e| {
-                                    let secs = e.as_secs();
-                                    if secs < 60 {
-                                        format!("{} secs ago", secs)
-                                    } else if secs < 3600 {
-                                        format!("{} mins ago", secs / 60)
-                                    } else if secs < 86400 {
-                                        format!("{} hours ago", secs / 3600)
-                                    } else {
-                                        format!("{} days ago", secs / 86400)
-                                    }
-                                })
-                                .unwrap_or_else(|| "unknown time".to_string());
-
-                            Some(format!(
-                                "{} files, {} dirs, {} (modified {})",
-                                file_count.bright_cyan(),
-                                dir_count.bright_green(),
-                                total_size.bright_yellow(),
-                                modified.bright_magenta()
-                            ))
-                        } else {
-                            None
+            Some(Message::FormatField(req)) => {
+                let entry = match req.entry {
+                    Some(e) => match lla_plugin_interface::DecoratedEntry::try_from(e) {
+                        Ok(entry) => entry,
+                        Err(e) => {
+                            return self.encode_error(&format!("Failed to convert entry: {}", e));
                         }
-                    }
-                    "default" => {
-                        if let (Some(file_count), Some(total_size)) = (
-                            entry.custom_fields.get("dir_file_count"),
-                            entry.custom_fields.get("dir_total_size"),
-                        ) {
-                            Some(format!("{} files, {}", file_count, total_size))
-                        } else {
-                            None
-                        }
-                    }
-                    _ => None,
+                    },
+                    None => return self.encode_error("Missing entry in format field request"),
                 };
-                PluginResponse::FormattedField(formatted)
+
+                if !entry.metadata.is_dir {
+                    Message::FieldResponse(proto::FormattedFieldResponse { field: None })
+                } else {
+                    let formatted = match req.format.as_str() {
+                        "long" => {
+                            if let (Some(file_count), Some(dir_count), Some(total_size)) = (
+                                entry.custom_fields.get("dir_file_count"),
+                                entry.custom_fields.get("dir_subdir_count"),
+                                entry.custom_fields.get("dir_total_size"),
+                            ) {
+                                let modified = entry
+                                    .path
+                                    .metadata()
+                                    .ok()
+                                    .and_then(|m| m.modified().ok())
+                                    .and_then(|t| t.elapsed().ok())
+                                    .map(|e| {
+                                        let secs = e.as_secs();
+                                        if secs < 60 {
+                                            format!("{} secs ago", secs)
+                                        } else if secs < 3600 {
+                                            format!("{} mins ago", secs / 60)
+                                        } else if secs < 86400 {
+                                            format!("{} hours ago", secs / 3600)
+                                        } else {
+                                            format!("{} days ago", secs / 86400)
+                                        }
+                                    })
+                                    .unwrap_or_else(|| "unknown time".to_string());
+
+                                Some(format!(
+                                    "{} files, {} dirs, {} (modified {})",
+                                    file_count.bright_cyan(),
+                                    dir_count.bright_green(),
+                                    total_size.bright_yellow(),
+                                    modified.bright_magenta()
+                                ))
+                            } else {
+                                None
+                            }
+                        }
+                        "default" => {
+                            if let (Some(file_count), Some(total_size)) = (
+                                entry.custom_fields.get("dir_file_count"),
+                                entry.custom_fields.get("dir_total_size"),
+                            ) {
+                                Some(format!("{} files, {}", file_count, total_size))
+                            } else {
+                                None
+                            }
+                        }
+                        _ => None,
+                    };
+                    Message::FieldResponse(proto::FormattedFieldResponse { field: formatted })
+                }
             }
-            PluginRequest::PerformAction(_, _) => PluginResponse::ActionResult(Ok(())),
-        }
+            Some(Message::Action(_req)) => Message::ActionResponse(proto::ActionResponse {
+                success: true,
+                error: None,
+            }),
+            _ => Message::ErrorResponse("Invalid request type".to_string()),
+        };
+
+        let response = proto::PluginMessage {
+            message: Some(response_msg),
+        };
+        let mut buf = bytes::BytesMut::with_capacity(response.encoded_len());
+        response.encode(&mut buf).unwrap();
+        buf.to_vec()
+    }
+}
+
+impl DirsPlugin {
+    fn encode_error(&self, error: &str) -> Vec<u8> {
+        use prost::Message;
+        let error_msg = lla_plugin_interface::proto::PluginMessage {
+            message: Some(
+                lla_plugin_interface::proto::plugin_message::Message::ErrorResponse(
+                    error.to_string(),
+                ),
+            ),
+        };
+        let mut buf = bytes::BytesMut::with_capacity(error_msg.encoded_len());
+        error_msg.encode(&mut buf).unwrap();
+        buf.to_vec()
     }
 }
 
