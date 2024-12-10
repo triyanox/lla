545cf7084b08710c50225f58cf6cd00cd35d67bb
Mohamed Achaq <motriyanox@gmail.com>
2024-12-10 08:33:08 +0100

    feat: migrate `file_tagger` plugin to a `Send` based architecture

--- plugins/file_tagger/src/lib.rs
+++ plugins/file_tagger/src/lib.rs
@@ -1,5 +1,5 @@
 use colored::Colorize;
-use lla_plugin_interface::{CliArg, DecoratedEntry, EntryDecorator, Plugin};
+use lla_plugin_interface::{Plugin, PluginRequest, PluginResponse};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
@@ -79,84 +79,108 @@ impl FileTaggerPlugin {
 }
 
 impl Plugin for FileTaggerPlugin {
-    fn version(&self) -> &'static str {
-        env!("CARGO_PKG_VERSION")
-    }
-
-    fn description(&self) -> &'static str {
-        env!("CARGO_PKG_DESCRIPTION")
-    }
-
-    fn cli_args(&self) -> Vec<CliArg> {
-        vec![CliArg {
-            name: "tag".to_string(),
-            short: Some('g'),
-            long: Some("tag".to_string()),
-            help: "Filter files by tag".to_string(),
-            takes_value: true,
-        }]
-    }
-
-    fn perform_action(&self, action: &str, args: &[String]) -> Result<(), String> {
-        let mut plugin = FileTaggerPlugin::new();
-        match action {
-            "add-tag" => {
-                if args.len() != 2 {
-                    return Err("Usage: add-tag <file_path> <tag>".to_string());
-                }
-                plugin.add_tag(&args[0], &args[1]);
-                Ok(())
+    fn handle_request(&mut self, request: PluginRequest) -> PluginResponse {
+        match request {
+            PluginRequest::GetName => PluginResponse::Name(env!("CARGO_PKG_NAME").to_string()),
+            PluginRequest::GetVersion => {
+                PluginResponse::Version(env!("CARGO_PKG_VERSION").to_string())
             }
-            "remove-tag" => {
-                if args.len() != 2 {
-                    return Err("Usage: remove-tag <file_path> <tag>".to_string());
-                }
-                plugin.remove_tag(&args[0], &args[1]);
-                Ok(())
+            PluginRequest::GetDescription => {
+                PluginResponse::Description(env!("CARGO_PKG_DESCRIPTION").to_string())
             }
-            "list-tags" => {
-                if args.len() != 1 {
-                    return Err("Usage: list-tags <file_path>".to_string());
-                }
-                let tags = plugin.get_tags(&args[0]);
-                println!("Tags for {}: {:?}", args[0], tags);
-                Ok(())
+            PluginRequest::GetSupportedFormats => {
+                PluginResponse::SupportedFormats(vec!["default".to_string()])
             }
-            "help" => Ok(println!(
-                "Available actions:\n\
-                 - add-tag <file_path> <tag>\n\
-                 - remove-tag <file_path> <tag>\n\
-                 - list-tags <file_path>"
-            )),
-            _ => Err(format!("Unknown action: {}", action)),
+            PluginRequest::Decorate(mut entry) => {
+                let tags = self.get_tags(entry.path.to_str().unwrap_or(""));
+                if !tags.is_empty() {
+                    entry
+                        .custom_fields
+                        .insert("tags".to_string(), tags.join(", "));
+                }
+                PluginResponse::Decorated(entry)
+            }
+            PluginRequest::FormatField(entry, _) => {
+                let formatted = entry.custom_fields.get("tags").map(|tags| {
+                    format!(
+                        "[{}]",
+                        tags.split(", ")
+                            .map(|t| t.cyan().to_string())
+                            .collect::<Vec<_>>()
+                            .join(", ")
+                    )
+                });
+                PluginResponse::FormattedField(formatted)
+            }
+            PluginRequest::PerformAction(action, args) => match action.as_str() {
+                "add-tag" => {
+                    if args.len() != 2 {
+                        println!("{} add-tag <file_path> <tag>", "Usage:".bright_cyan());
+                        return PluginResponse::ActionResult(Ok(()));
+                    }
+                    self.add_tag(&args[0], &args[1]);
+                    println!(
+                        "{} tag '{}' to {}",
+                        "Added".bright_green(),
+                        args[1].cyan(),
+                        args[0].bright_blue()
+                    );
+                    PluginResponse::ActionResult(Ok(()))
+                }
+                "remove-tag" => {
+                    if args.len() != 2 {
+                        println!("{} remove-tag <file_path> <tag>", "Usage:".bright_cyan());
+                        return PluginResponse::ActionResult(Ok(()));
+                    }
+                    self.remove_tag(&args[0], &args[1]);
+                    println!(
+                        "{} tag '{}' from {}",
+                        "Removed".bright_green(),
+                        args[1].cyan(),
+                        args[0].bright_blue()
+                    );
+                    PluginResponse::ActionResult(Ok(()))
+                }
+                "list-tags" => {
+                    if args.len() != 1 {
+                        println!("{} list-tags <file_path>", "Usage:".bright_cyan());
+                        return PluginResponse::ActionResult(Ok(()));
+                    }
+                    let tags = self.get_tags(&args[0]);
+                    if tags.is_empty() {
+                        println!(
+                            "{} No tags found for {}",
+                            "Info:".bright_blue(),
+                            args[0].bright_yellow()
+                        );
+                    } else {
+                        println!("{} for {}:", "Tags".bright_green(), args[0].bright_blue());
+                        for tag in tags {
+                            println!("  {} {}", "→".bright_cyan(), tag.bright_yellow());
+                        }
+                    }
+                    PluginResponse::ActionResult(Ok(()))
+                }
+                "help" => {
+                    println!("{}", "File Tagger Commands".bright_green().bold());
+                    println!("");
+                    println!("{}", "Available actions:".bright_yellow());
+                    println!("  {} <file_path> <tag>", "add-tag".bright_cyan());
+                    println!("    Add a tag to a file");
+                    println!("");
+                    println!("  {} <file_path> <tag>", "remove-tag".bright_cyan());
+                    println!("    Remove a tag from a file");
+                    println!("");
+                    println!("  {} <file_path>", "list-tags".bright_cyan());
+                    println!("    List all tags for a file");
+                    PluginResponse::ActionResult(Ok(()))
+                }
+                _ => {
+                    println!("{} Unknown action: {}", "Error:".bright_red(), action);
+                    PluginResponse::ActionResult(Ok(()))
+                }
+            },
         }
-    }
-}
-
-impl EntryDecorator for FileTaggerPlugin {
-    fn name(&self) -> &'static str {
-        env!("CARGO_PKG_NAME")
-    }
-
-    fn decorate(&self, entry: &mut DecoratedEntry) {
-        let tags = self.get_tags(entry.path.to_str().unwrap_or(""));
-        if !tags.is_empty() {
-            entry
-                .custom_fields
-                .insert("tags".to_string(), tags.join(", "));
-        }
-    }
-
-    fn format_field(&self, entry: &DecoratedEntry, _format: &str) -> Option<String> {
-        entry.custom_fields.get("tags").map(|tags| {
-            format!(
-                "[{}]",
-                tags.split(", ")
-                    .map(|t| t.cyan().to_string())
-                    .collect::<Vec<_>>()
-                    .join(", ")
-            )
-        })
     }
 }
 
